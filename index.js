b67f0cea5a7c5b677bdcf1a3d70103b752aa2a9c
tkdlxm457 <tkdlxm457@naver.com>
2021-09-08 15:00:27 +0900

    add connection server

---
+++ index.js
@@ -0,0 +1,126 @@
+/**
+ * 1. 토픽 목록을 불러온다.
+ *  1.1. 불러온 데이터를 table에 렌더링한다.
+ *  1.2. click 함수를 추가해서 선택된 row의 데이터를 객체에 저장한다.
+ *  1.3. 선택된 row의 데이터를 입력상자에 바인딩 한다.
+ * 
+ * 2. 신규 데이터를 저장 요청한다.
+ *  2.1. 입력상자에 입력 된 값을 서버에 저장요청 한다.
+ * 
+ * 3. row를 업데이트 한다.
+ *  3.1. (1.2, 1.3)을 진행한다.
+ *  3.2. 서버에 업데이트를 요청한다.
+ * 
+ * 4. row를 삭제한다.
+ *  4.1. (1.2, 1.3)을 진행한다.
+ *  4.2. 서버에 삭제를 요청한다.
+ */
+ // http://tcpschool.com/jquery/jq_ajax_method
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class tv {
+    private volumn = 0;
+    private channel = 0;
+
+    getVolumn() {
+        return this.volumn;
+    }
+
+    setVolumn(volumn) {
+        this.volumn = volumn;
+    }
+
+    getChannel() {
+        return this.channel;
+    }
+}
+
+
+let lgTv = new tv();
+
+lgTv.getVolumn();
+
+
+lgTv.getVolumn();
+
+
+
+
+
+
+
+
+// let topics = [];
+
+// function getTopics() {
+//     // XMLHttpRequest......
+
+//     // [TODO] ajax 구현 후 제거
+//     topics = [
+//         { id: 1, title: "타이틀입니다1.", description: "설명입니다1.", author_id: 1, create: "2021-07-08" },
+//         { id: 2, title: "타이틀입니다2.", description: "설명입니다2.", author_id: 1, create: "2021-07-08" },
+//         { id: 3, title: "타이틀입니다3.", description: "설명입니다3.", author_id: 1, create: "2021-07-08" },
+//         { id: 4, title: "타이틀입니다4.", description: "설명입니다4.", author_id: 1, create: "2021-07-08" },
+//     ];
+
+
+//     // 1.1
+//     var insertTr;
+
+//     topics.forEach(topic => {
+//         insertTr = ""
+
+//         insertTr += `<tr onclick='selectRow(topic)'>`;
+//         insertTr += `<td>${topic.id}</td>`;
+//         insertTr += "<td>이순신</td>";
+//         insertTr += "<td>부산</td>";
+//         insertTr += "</tr>";
+    
+//         $("#tbody").append(insertTr);
+//     });
+    
+
+
+// }
+
+// function selectRow() {
+
+// }
+
+// function saveTopic() {
+
+// }
+
+// function updateTopic() {
+
+// }
+
+// function deleteTopic() {
+
+// }
+
+
+
+
+
